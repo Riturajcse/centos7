969319ecb723c033df0b4f436cde358e5ccdb0e6
Riturajcse <riturajcse1994@gmail.com>
2018-02-16 15:03:32 +0530

    adds initial files

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+echo "HOME=$HOME"
+cd /c7
+
+echo "================= Updating package lists ==================="
+yum clean expire-cache
+yum check-update || true
+
+echo "================= Adding some global settings ==================="
+mv gbl_env.sh /etc/profile.d/
+mkdir -p "$HOME/.ssh/"
+mv config "$HOME/.ssh/"
+mv 90forceyes /etc/apt/apt.conf.d/
+touch "$HOME/.ssh/known_hosts"
+
+echo "================= Installing basic packages ==================="
+yum -y install -q \
+  sudo \
+  gcc \
+  gcc-c++ \
+  kernel-devel \
+  make
